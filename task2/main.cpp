44e8000610474b11618ec27193d7020525b26a57
Daria Kolodzey <kolodaria@google.com>
2016-02-24 17:41:34 +0300

    task2 :: Fix summator, Add makefile

--- task2/main.cpp
+++ task2/main.cpp
@@ -6,29 +6,30 @@
 #include <chrono>
 #include <iomanip>
 #include <ctime>
+#include <random>
 
 using namespace std;
 
 
-void sum(vector<double> &array, int from, int to,
+void sum(vector<double> &arr, int from, int to,
 		double &common_sum, mutex &m, bool need_sin) {
 	double my_sum = 0;
 	for (int i = from; i < to; ++i) {
-		my_sum += need_sin ? sin(array[i]) : array[i];
+		my_sum += need_sin ? sin(arr[i]) : arr[i];
 	}
-	mutex.lock();
+	m.lock();
 	common_sum += my_sum;
-	mutex.unlock();
+	m.unlock();
 }
 
-vector<double> array;
+vector<double> arr;
 
 int main () {
 	int arrSize = 100000000;
-	std::mt19937 gen;
-	uniform_real_distribution distr(-1000000000, 1000000000);
+    std::mt19937 gen;
+	uniform_real_distribution<> distr(-1000000000, 1000000000);
 	for (int i = 0; i < arrSize; ++i) {
-		array.push_back(distr(gen));
+		arr.push_back(distr(gen));
 	}
 	int Nthreads;
 	cin >> Nthreads;
@@ -38,20 +39,18 @@ int main () {
 	vector<thread> t;
 	mutex m;
 
-	chrono::system_clock::time_point start = chrono::steady_clock::now();
+	chrono::steady_clock::time_point start = chrono::steady_clock::now();
 	for (int i = 0; i < Nthreads; ++i) {
 		t.push_back(thread(sum, 
-			ref(array),
+			ref(arr),
 			(arrSize / Nthreads) * i,
 			(arrSize / Nthreads) * (i + 1), 
 			ref(ans), ref(m), need_sin));
 	}
 	for (int i = 0; i < t.size(); ++i)
 		t[i].join();
-	chrono::system_clock::time_point end = chrono::steady_clock::now();
-	cout << "Summing took "
-         << chrono::duration_cast<std::chrono::microseconds>(end - start).count()
-         << "us.\n";
+	chrono::steady_clock::time_point end = chrono::steady_clock::now();
+	cout << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << endl;
 
 	return 0;
 }
