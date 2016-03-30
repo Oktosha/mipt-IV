2f436981f60ed9f350e43784892bdb6d0d267556
Daria Kolodzey <kolodaria@google.com>
2016-03-30 18:56:08 +0300

    Segment tree implementation

--- task6/main-l.cpp
+++ task6/main-l.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <cassert>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,7 +20,7 @@ struct Request {
 	int pos;
 	Request(int t, int n, int k, int l, int pos)
 		: t(t), n(n), k(k), l(l), pos(pos) {}
-	bool operator < (Request r) {
+	bool operator < (Request r) const {
 		return pos < r.pos;
 	}
 	
@@ -25,30 +28,87 @@ struct Request {
 
 
 struct RSQ {
-	RSQ* left;
-	RSQ* right;
+	RSQ(int l, int r): l(l), r(r), left(nullptr), right(nullptr), sum(0) {}
 	int l;
 	int r;
+	RSQ* left;
+	RSQ* right;
 	int sum;
 };
 
-RSQ* build();
+RSQ* build(int l, int r) {
+	RSQ* root = new RSQ(l, r);
+	if (l < r) {
+		root->left = build(l, (l + r)/2);
+		root->right = build(((l + r) / 2) + 1, r);
+	}
+	return root;
+}
+
+int sum(RSQ* root, int l, int r) {
+	assert((root->l <= l) && (r <= root->r));
+	if ((root->l == l) && (root->r == r))
+		return root->sum;
+	int ans = 0;
+	if (l <= root->left->r) {
+		ans += sum(root->left, l, min(r, root->left->r));
+	}
+	if (r >= root->right->l) {
+		ans += sum(root->right, max(l, root->right->l), r);
+	}
+	return ans;
+}
+
+void inc(RSQ* root, int x) {
+	//cout << "l r x " << root->l << " " << root->r << " " << x << endl;
+	assert((root->l <= x) && (x <= root->r));
+	root->sum += 1;
+	if (root->l == root->r) {
+		return;
+	}
+	if (x <= root->left->r) {
+		inc(root->left, x);
+	} else {
+		inc(root->right, x);
+	}
+}
 
 int main() {
-	cin >> N >> M;
+	ifstream fin;
+	ofstream fout;
+	fin.open("permutation.in");
+	fout.open("permutation.out", std::ofstream::out);
+	ios_base::sync_with_stdio(0);
+	int N, M;
+	fin >> N >> M;
 	perm[0] = 0;
 	for (int i = 1; i <= N; ++i) {
-		cin >> perm[i];
+		fin >> perm[i];
 	}
 	vector<Request> request;
 	for (int i = 0; i <= M; ++i) {
 		int x, y, k, l;
-		cin >> x >> y >> k >> l;
-		request.emplace_back(t, i, k, l, x - 1);
-		request.emplace_back(t, i, k, l, y);
+		fin >> x >> y >> k >> l;
+		request.emplace_back(0, i, k, l, x - 1);
+		request.emplace_back(1, i, k, l, y);
 	}
 
 	sort(request.begin(), request.end());
+	RSQ *rsq = build(1, N);
+	int cur_pos = 0;
+	for (auto req : request) {
+		while(cur_pos < req.pos) {
+			++cur_pos;
+			//cout << "inc " << perm[cur_pos] << endl;
+			inc(rsq, perm[cur_pos]);
+		}
+		a[req.n][req.t] = sum(rsq, req.k, req.l);
+	}
 
+	for (int i = 0; i < M; ++i) {
+		fout << a[i][1] - a[i][0] << "\n";
+	}
+	fin.close();
+	fout.close();
 	return 0;
 }
